ab3037d76529e051857e88c8edea80712adcff63
AugustoFKL <augusto.fotino@hotmail.com>
2021-10-17 23:18:50 -0300

    #noissue First commit

---
+++ src/main/java/com/example/habiticaapiconsumer/HabiticaApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.habiticaapiconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabiticaApiConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabiticaApiConsumerApplication.class, args);
+    }
+
+}
